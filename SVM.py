f8b5063059d47b15a3b92a9a219e65cd9f1da7e5
Qazzzie <santangelozach@gmail.com>
2021-05-19 12:17:13 -0700

    created model files

---
+++ SVM.py
@@ -0,0 +1,18 @@
+"""
+    SVM Star classifier
+
+"""
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import scipy.stats
+import numpy as np
+import csv 
+
+
+class SVM:
+    __init__():
+
+
